078f740ad93c878712dcc33a016d1dfabfe69e6f
ohprettyhak <haklee.googl@gmail.com>
2022-02-24 16:10:42 +0900

    5337: 웰컴

---
+++ BOJ/5000/5337.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    printf(".  .   .\n");
+    printf("|  | _ | _. _ ._ _  _\n");
+    printf("|/\\|(/.|(_.(_)[ | )(/.\n");
+    
+    return 0;
+}
