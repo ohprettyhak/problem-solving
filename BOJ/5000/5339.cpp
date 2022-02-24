358938d8a12a57e3e45569b0addf5c52ca178f46
ohprettyhak <haklee.googl@gmail.com>
2022-02-24 16:13:58 +0900

    5339: 콜센터

---
+++ BOJ/5000/5339.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    printf("     /~\\\n");
+    printf("    ( oo|\n");
+    printf("    _\\=/_\n");
+    printf("   /  _  \\\n");
+    printf("  //|/.\\|\\\\\n");
+    printf(" ||  \\ /  ||\n");
+    printf("============\n");
+    printf("|          |\n");
+    printf("|          |\n");
+    printf("|          |\n");
+    
+    return 0;
+}
