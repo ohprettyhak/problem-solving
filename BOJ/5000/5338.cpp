e7e6145a080c7fbef4a6d4f73f042772103722f5
ohprettyhak <haklee.googl@gmail.com>
2022-02-24 16:12:08 +0900

    5338: 마이크로소프트 로고

---
+++ BOJ/5000/5338.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    printf("       _.-;;-._\n");
+    printf("'-..-'|   ||   |\n");
+    printf("'-..-'|_.-;;-._|\n");
+    printf("'-..-'|   ||   |\n");
+    printf("'-..-'|_.-''-._|");
+    
+    return 0;
+}
