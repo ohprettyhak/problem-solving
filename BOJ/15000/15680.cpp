20656bffe9ac952a6598fe79e627522d1fff40ba
ohprettyhak <haklee.googl@gmail.com>
2022-06-24 16:13:58 +0900

    15680: 연세대학교

---
+++ BOJ/15000/15680.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if (n) cout << "Leading the Way to the Future";
+    else cout << "YONSEI";
+    
+    return 0;
+}
