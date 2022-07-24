c5bec4dc1e6358bc6fb73cef99fa61320eefb144
ohprettyhak <haklee.googl@gmail.com>
2022-07-24 22:10:45 +0900

    6810: ISBN

---
+++ BOJ/6000/6810.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << "The 1-3-sum is " << 91 + a * 1 + b * 3 + c * 1;
+    
+    return 0;
+}
