0db1263282a989a13f69ed592f390a539e2e2a71
ohprettyhak <haklee.googl@gmail.com>
2022-07-25 15:43:18 +0900

    8674: Tabliczka

---
+++ BOJ/8000/8674.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    long long a, b;
+    cin >> a >> b;
+
+    if (!(a % 2) || !(b % 2)) cout << 0;
+    else cout << min(a, b);
+
+    return 0;
+}
