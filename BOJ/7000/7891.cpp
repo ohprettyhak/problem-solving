01078802868e279a4930023473a7c972e96129e5
ohprettyhak <haklee.googl@gmail.com>
2022-07-25 15:34:04 +0900

    7891: Can you add this?

---
+++ BOJ/7000/7891.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+
+    return 0;
+}
