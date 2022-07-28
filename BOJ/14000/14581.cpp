966340166f6826434120b982e8831d0c3f9048d5
ohprettyhak <haklee.googl@gmail.com>
2022-07-28 22:42:41 +0900

    14581: 팬들에게 둘러싸인 홍준

---
+++ BOJ/14000/14581.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    string word;
+    cin >> word;
+
+    cout << ":fan::fan::fan:" << '\n';
+    cout << ":fan::" << word << "::fan:" << '\n';
+    cout << ":fan::fan::fan:" << '\n';
+
+    return 0;
+}
