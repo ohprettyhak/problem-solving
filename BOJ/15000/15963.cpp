dfd5bfb06f156fd3e678ba75c8cedac092af1367
ohprettyhak <haklee.googl@gmail.com>
2022-08-29 23:29:23 +0900

    15963: CASIO

---
+++ BOJ/15000/15963.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    long long n, m;
+    cin >> n >> m;
+
+    cout << (n == m) << '\n';
+    
+    return 0;
+}
