365429a85b3500ce5ac04ae44debc5a85a11a643
ohprettyhak <haklee.googl@gmail.com>
2022-09-02 23:23:31 +0900

    2442: 별 찍기 - 5

---
+++ BOJ/2000/2442.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) cout << " ";
+        for (int j = 1; j <= 2 * i - 1; j++) cout << "*";
+        cout << '\n';
+    }
+    
+    return 0;
+}
