0e1ec9e46336a2a0faefecc2aa218286bf7f6218
ohprettyhak <haklee.googl@gmail.com>
2022-09-02 23:04:53 +0900

    24723: 녹색거탑

---
+++ BOJ/24000/24723.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << pow(2, n) << '\n';
+
+    return 0;
+}
