f06f418c41fa01af7394fd2aef2942d791bca62f
ohprettyhak <haklee.googl@gmail.com>
2022-09-25 21:01:46 +0900

    1302: 베스트셀러

---
+++ BOJ/1000/1302.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int main() {
+    FASTIO
+
+    vector<string> books;
+    int n;
+    cin >> n;
+
+    books.resize(n);
+    for (int i = 0; i < n; i++) cin >> books[i];
+    sort(books.begin(), books.end());
+
+    int range = 1, rstRange = 1;
+    string prev = "", rst = books[0];
+    for (int i = 0; i < n; i++) {
+        if (prev == books[i]) {
+            range++;
+            if (rstRange < range) {
+                rst = books[i];
+                rstRange = range;
+            }
+        } else range = 1;
+        prev = books[i];
+    }
+
+    cout << rst << '\n';
+    
+    return 0;
+}
