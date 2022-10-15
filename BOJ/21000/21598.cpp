5d934dc91aba85d98038a70c6f0909b87b1b78d0
ohprettyhak <haklee.googl@gmail.com>
2022-10-15 21:12:52 +0900

    21598: SciComLove

---
+++ BOJ/21000/21598.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int main() {
+    FASTIO;
+
+    int t;
+    cin >> t;
+
+    while (t--) cout << "SciComLove" << '\n';
+    
+    return 0;
+}
