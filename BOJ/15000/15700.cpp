9833f64928461ab4a9aca7519b2640fc86c4ca13
ohprettyhak <haklee.googl@gmail.com>
2022-10-16 11:22:51 +0900

    15700: 타일 채우기 4

---
+++ BOJ/15000/15700.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int main() {
+    FASTIO;
+
+    long long n, m;
+    cin >> n >> m;
+
+    cout << n * m / 2 << '\n';
+    
+    return 0;
+}
