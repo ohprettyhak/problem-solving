c0dbc8a00ae541cbc73e7364df489ab48ca8ae83
ohprettyhak <haklee.googl@gmail.com>
2022-10-16 11:12:06 +0900

    25179: 배스킨라빈스~N~귀엽고~깜찍하게~

---
+++ BOJ/25000/25179.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int main() {
+    FASTIO;
+
+    long long n, m;
+    cin >> n >> m;
+
+    long long rst = n % (m + 1);
+    if (rst == 1) cout << "Can't win" << '\n';
+    else cout << "Can win" << '\n';
+    
+    return 0;
+}
