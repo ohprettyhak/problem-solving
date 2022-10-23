608b2c6daad67c6820cfcaf913fe839b75d4cef0
ohprettyhak <haklee.googl@gmail.com>
2022-10-23 04:12:01 +0000

    24263: 알고리즘 수업 - 알고리즘의 수행 시간 2

---
+++ BOJ/24000/24263.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int main() {
+    FASTIO;
+
+    int n;
+    cin >> n;
+
+    cout << n << '\n';
+    cout << 1 << '\n';
+    
+    return 0;
+}
