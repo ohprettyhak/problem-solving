a8a6c218d557c5a7244373e552d41cecae26260b
ohprettyhak <haklee.googl@gmail.com>
2022-12-04 13:51:58 +0900

    1924: 최소 힙

---
+++ BOJ/Kotlin/1000/1927.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {    
+    val n = readln().toInt()
+    val queue = PriorityQueue<Int>()
+
+    repeat (n) {
+        val x = readln().toInt()
+        if (x == 0) {
+            if (queue.isEmpty()) println("0")
+            else println(queue.poll())
+        } else queue.add(x)
+    }
+}
