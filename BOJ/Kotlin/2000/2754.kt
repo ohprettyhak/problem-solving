0ad6321db879a575e66688b71dd8371d9daff30b
ohprettyhak <haklee.googl@gmail.com>
2022-12-12 15:52:24 +0900

    2754: 학점계산

---
+++ BOJ/Kotlin/2000/2754.kt
@@ -0,0 +1,8 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readln()
+
+    val score = mutableMapOf<String, Double>("A+" to 4.3, "A0" to 4.0, "A-" to 3.7, "B+" to 3.3, "B0" to 3.0, "B-" to 2.7, "C+" to 2.3, "C0" to 2.0, "C-" to 1.7, "D+" to 1.3, "D0" to 1.0, "D-" to 0.7, "F" to 0.0)
+
+    val result = "%.1f".format(score[n]!!)
+    println(result)
+}
