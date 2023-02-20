6587d49c4270941b687665c408004dc0375c65a2
ohprettyhak <haklee.googl@gmail.com>
2023-02-20 22:11:24 +0900

    27465: 소수가 아닌 수

---
+++ BOJ/27000/27465.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    FASTIO;
+    
+	int n;
+    cin >> n;
+    cout << 1000000000;
+    return 0;
+}
