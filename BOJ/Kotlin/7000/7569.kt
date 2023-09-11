b5834637cc3463ff34df90a9fd0b335b14b0b3ba
ohprettyhak <haklee.googl@gmail.com>
2023-09-11 23:04:00 +0900

    7569: 토마토

--- BOJ/Kotlin/7000/7569.kt
+++ BOJ/Kotlin/7000/7569.kt
@@ -1,53 +1,48 @@
-import java.util.LinkedList
+import java.util.*
 
-data class Tomato(val x: Int, val y: Int, val z: Int)
-
-val dx = arrayOf(-1, 1, 0, 0, 0, 0)
-val dy = arrayOf(0, 0, -1, 1, 0, 0)
-val dz = arrayOf(0, 0, 0, 0, -1, 1)
+val dx = listOf(0, 0, 1, -1, 0, 0)
+val dy = listOf(1, -1, 0, 0, 0, 0)
+val dz = listOf(0, 0, 0, 0, 1, -1)
 
 fun main() = with(System.`in`.bufferedReader()) {
-    // n: row, m: column, h: height
-    val (m, n, h) = readln().split(" ").map { it.toInt() }
-    val box = Array<Array<Array<Int>>>(h) { Array(n) { Array(m, {0})} }
+    val (m, n, h) = readln().split(" ").map { it.toInt() }  // m: 가로, n: 세로, h: 높이
+    val box = Array(h) { Array(n) { IntArray(m) } }
+    val queue: Queue<Triple<Int, Int, Int>> = LinkedList()
 
-    val queue = LinkedList<Tomato>()
-    for (z in 0 until h) {
-        for (y in 0 until n) {
+    repeat(h) { z ->
+        repeat(n) { y ->
             val line = readln().split(" ").map { it.toInt() }
-            line.forEachIndexed { x, it ->
-                box[z][y][x] = it
-                if (it == 1) queue.add(Tomato(x, y, z))  // x, y, z
+            line.forEachIndexed { x, value ->
+                box[z][y][x] = value
+                if (value == 1) queue.add(Triple(x, y, z))
             }
         }
     }
 
     while (queue.isNotEmpty()) {
-        val t:Tomato = queue.poll()
-        
-        for (i in 0..5) {
-            val dx = t.x + dx[i]
-            val dy = t.y + dy[i]
-            val dz = t.z + dz[i]
-            if (dx >= 0 && dx < m && dy >= 0 && dy < n && dz >= 0 && dz < h && box[dz][dy][dx] == 0) {
-                queue.add(Tomato(dx, dy, dz))
-                box[dz][dy][dx] = box[t.z][t.y][t.x] + 1
-            }
+        val (x, y, z) = queue.poll()
+
+        for (i in 0 until 6) {
+            val mx = x + dx[i]
+            val my = y + dy[i]
+            val mz = z + dz[i]
+
+            if (mx < 0 || mx >= m || my < 0 || my >= n || mz < 0 || mz >= h || box[mz][my][mx] != 0) continue
+            box[mz][my][mx] = box[z][y][x] + 1
+            queue.add(Triple(mx, my, mz))
         }
     }
 
     var result = 0
-    for (z in 0 until h) {
+    loop@ for (z in 0 until h) {
         for (y in 0 until n) {
             for (x in 0 until m) {
-                if (box[z][y][x] == 0) {
-                    println(-1)
-                    return
-                }
-                result = Math.max(result, box[z][y][x])
+                if (box[z][y][x] == 0) result = -1
+                if (result == -1) break@loop
+                result = result.coerceAtLeast(box[z][y][x])
             }
         }
     }
-    if (result == 1) println(0)
-    else println(result - 1)
+
+    println(if (result == -1) -1 else result - 1)
 }
